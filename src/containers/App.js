ec759092c6d8dd585de1b9f66142ef5accae2139
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-01 20:24:03 +0600

    initial structure setup

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Buddy!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
