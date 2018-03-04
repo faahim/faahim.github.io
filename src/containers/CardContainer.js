d88931db1bd04e9596a1f51e1219e7e303fc57ed
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-04 22:46:46 +0600

    implimented logic to handle rendering apps

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -11,12 +11,34 @@ class CardContainer extends Component {
   }
 
   render() {
+    const {clickHandler} = this.props;
+    const allCards = [
+     {
+       name: "about",
+       component: <AboutCard onClick={clickHandler}/>
+     },
+     {
+       name: "projects",
+       component: <ProjectsCard onClick={clickHandler}/>
+     },
+     {
+       name: "writings",
+       component: <WritingsCard onClick={clickHandler}/>
+     },
+     {
+       name: "contact",
+       component: <ContactsCard onClick={clickHandler}/>
+     }
+    ];
+
     return(
       <div className="card-container">
-        <AboutCard/>
-        <ProjectsCard/>
-        <WritingsCard/>
-        <ContactsCard/>
+        {this.props.choosenCard == null && allCards.map(item => item.component)}
+        {this.props.choosenCard && allCards.map(item => {
+          if(item.name == this.props.choosenCard) {
+            return item.component
+          }
+        })}
       </div>
     )
   }
