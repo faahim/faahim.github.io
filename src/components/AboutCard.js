a209a2a4393e9beaafb906ebaea9def4773eb188
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-04 22:45:43 +0600

    removed svg component and imports

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Svg1 } from './Svgs';
 import '../styles/AboutCard.css';
 
 function AboutCard(props) {
   return(
-    <div className="card about-card">
+    <div className="card about-card" onClick={props.onClick('about')}>
       <h3>Hi there!</h3>
       <h1>I'm Faahim. I love working on Front-End!</h1>
       <p>Click to learn more about me. :)</p>
-      <Svg1/>
     </div>
   )
 }
--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Svg2 } from './Svgs';
 import '../styles/ProjectsCard.css';
 
 function ProjectCard(props) {
   return(
-    <div className="card projects-card">
+    <div className="card projects-card" onClick={props.onClick('projects')}>
       <h2>Projects I've worked on</h2>
       <p>Checkout some of the projects I've loved working on</p>
-      <Svg2 />
     </div>
   )
 }
