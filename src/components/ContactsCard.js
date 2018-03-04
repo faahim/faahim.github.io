beadc2482c2e7f99eb17f4d5010f5113bd9f8a03
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-04 22:46:02 +0600

    added click events

--- src/components/ContactsCard.js
+++ src/components/ContactsCard.js
@@ -3,7 +3,7 @@ import '../styles/ContactsCard.css';
 
 function ContactsCard(props) {
   return(
-    <div className="card contacts-card">
+    <div className="card contacts-card" onClick={props.onClick('contact')}>
       <h2>Get in touch!</h2>
       <p>I love to meet new people from around the world! Let's disscuss about some projects idea or how cool your hometown is!</p>
     </div>
--- src/components/WritingsCard.js
+++ src/components/WritingsCard.js
@@ -3,7 +3,7 @@ import '../styles/WritingsCard.css';
 
 function WritingsCard(props) {
   return(
-    <div className="card writings-card">
+    <div className="card writings-card" onClick={props.onClick('writings')}>
       <h2>My Writings</h2>
       <p>I love to writing about my experience. Check some of them out!</p>
     </div>
