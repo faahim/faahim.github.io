04522dbb53986d26b3f62eaa6002cea1c01ae719
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-04 22:46:20 +0600

    tweaked svgc colors

--- src/components/Svgs.js
+++ src/components/Svgs.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 export const Svg1 = (props) => {
   return(
-    <svg width="120" height="120" viewBox="0 0 1400 1400" version="1.1" xmlns="http://www.w3.org/2000/svg">
-      <title>Ellipse</title>
+    <svg className="about-svg" width="120" height="120" viewBox="0 0 1400 1400" version="1.1" xmlns="http://www.w3.org/2000/svg">
       <g id="Canvas" transform="translate(928 712)">
       <g id="Ellipse">
       </g>
       </g>
       <linearGradient id="paint1_linear" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-1288 980 -980 -1288 1830 676)">
-      <stop offset="0" stop-color="#4DFFBF70"/>
-      <stop offset="1" stop-color="#0E6C8A70" stop-opacity="0.4"/>
+      <stop offset="0" stop-color="#4DFFBF"/>
+      <stop offset="1" stop-color="#0A82A8"/>
       </linearGradient>
       <path fill="url(#paint1_linear)" d="M 1203.84 900.353C 1203.84 1173.57 1573.1 1400 1298.36 1400C 1023.62 1400 0 1173.57 0 900.353C 0 627.138 1023.62 0 1298.36 0C 1573.1 0 1203.84 627.138 1203.84 900.353Z"/>
     </svg>
@@ -19,17 +18,16 @@ export const Svg1 = (props) => {
 
 export const Svg2 = (props) => {
   return(
-    <svg width="120" height="120" viewBox="0 0 1132 1261" version="1.1" xmlns="http://www.w3.org/2000/svg">
-      <title>Ellipse 2</title>
-      <g id="Canvas" transform="translate(1544 1749)">
-        <g id="Ellipse 2">
-        </g>
-      </g>
-      <linearGradient id="paint2_linear" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(856.001 -732 771.783 902.521 153.154 745.494)">
-      <stop offset="0" stop-color="#F26F8170"/>
-      <stop offset="1" stop-color="#996A7070" stop-opacity="0.65"/>
-      </linearGradient>
-      <path fill="url(#paint2_linear)" d="M 1131.05 108.756C 1131.05 413.617 912.476 1260.16 591.046 1260.16C 269.617 1260.16 67.0459 1177.62 67.0459 872.756C 67.0459 567.895 -142.384 236.756 179.046 236.756C 500.476 236.756 1131.05 -196.105 1131.05 108.756Z"/>
+    <svg className="projects-svg" width="150" height="150" viewBox="0 0 1132 1261" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <g id="Canvas" transform="translate(1544 1749)">
+    <g id="Ellipse 2">
+    </g>
+    </g>
+    <linearGradient id="paint2_linear" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(852 -1132 1193.52 898.303 -317.715 715.604)">
+    <stop offset="0" stop-color="#FC4961"/>
+    <stop offset="1" stop-color="#6E0D1A" stop-opacity="0.65"/>
+    </linearGradient>
+    <path fill="url(#paint2_linear)" d="M 1131.05 108.756C 1131.05 413.617 912.476 1260.16 591.046 1260.16C 269.617 1260.16 67.0459 1177.62 67.0459 872.756C 67.0459 567.895 -142.384 236.756 179.046 236.756C 500.476 236.756 1131.05 -196.105 1131.05 108.756Z"/>
     </svg>
   )
 }
