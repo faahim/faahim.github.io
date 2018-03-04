36d5c3487e6fcb61096c2261a95340263cdf7937
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-04 22:47:10 +0600

    implemented click handler and added svgs

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CardContainer from './CardContainer';
+import { Svg1, Svg2} from '../components/Svgs';
 import '../styles/App.css';
 
 class App extends Component {
@@ -7,14 +8,29 @@ class App extends Component {
     super();
 
     this.state = {
-      app: <CardContainer/>
+      cardSelected: false,
+      choosenCard: null
     }
+
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler = param => event => {
+    this.setState({
+      cardSelected: true,
+      choosenCard: param
+    }, () => {
+      const cardSvg = document.getElementsByClassName(this.state.choosenCard+"-svg")[0];
+      cardSvg.classList.add("expand");
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        {this.state.app}
+      <div className={this.state.cardSelected ? "App single-card" : "App"}>
+        <CardContainer clickHandler={this.clickHandler} choosenCard={this.state.choosenCard}/>
+        <Svg1/>
+        <Svg2/>
       </div>
     );
   }
