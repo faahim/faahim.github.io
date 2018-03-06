5e921f9c2e6beff4fbd219725a43ab7ffa6e9471
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-06 22:52:09 +0600

    removed heading tags

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../styles/AboutCard.css';
+import Anime from 'react-anime';
 
 function AboutCard(props) {
   return(
+    <Anime easing="easeInSine" opacity={[0, 1]} scale={[1.3, 1]} delay={100} duration={300}>
     <div className="card about-card" onClick={props.onClick('about')}>
-      <h3>Hi there!</h3>
-      <h1>I'm Faahim. I love working on Front-End!</h1>
+      <p>Hi there!</p>
+      <p>I'm Faahim. I love working on Front-End!</p>
       <p>Click to learn more about me. :)</p>
     </div>
+    </Anime>
   )
 }
 
