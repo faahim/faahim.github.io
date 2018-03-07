634dd2327097a3e7a8f8df418c12185be5be410c
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-07 17:30:59 +0600

    added props for transition array

--- src/components/ContactsCard.js
+++ src/components/ContactsCard.js
@@ -4,7 +4,7 @@ import Anime from 'react-anime';
 
 function ContactsCard(props) {
   return(
-    <Anime easing="easeInSine" scale={[0.7, 1]} opacity={[0, 1]} delay={300} duration={300}>
+    <Anime easing="easeInSine" scale={props.scaleArr} opacity={[0, 1]} delay={300} duration={300}>
       <div className="card contacts-card" onClick={props.onClick('contact')}>
         <h2>Get in touch!</h2>
         <p>I'm friendly, Let's talk!</p>
