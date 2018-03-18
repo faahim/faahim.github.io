2a8cb8cc720c7b4179bc22fc55c191caff407d2a
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-03-18 17:43:29 +0600

    fixed gramatical error in text

--- src/components/WritingsCard.js
+++ src/components/WritingsCard.js
@@ -7,7 +7,7 @@ function WritingsCard(props) {
     <Anime easing="easeInSine" scale={props.scaleArr} opacity={[0, 1]} delay={700} duration={300}>
       <div className="card writings-card" onClick={props.onClick('writings')} onMouseEnter={props.MouseEnter('writings')} onMouseLeave={props.MouseLeave('writings')}>
         <h2>Writings</h2>
-        <p>Check some of my writings!</p>
+        <p>Check out some of my writings!</p>
       </div>
     </Anime>
   )
