09604a9ef1b288fd2b49228dd64b70d7ebb54662
Faahim <afiur.fahim@gmail.com>
2025-11-09 16:39:16 +0600

    more copy updates

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -18,10 +18,12 @@ const AboutPage = props => {
               <p>Hi there! <span className="hand-wave">👋</span></p>
               <h1>I'm Afiur Rahman Fahim.</h1>
               <p>You can call me <strong>Faahim.</strong> :)</p>
-              <p>I'm a full-stack developer based in Bangladesh. I specialize in JavaScript and my main area of interest is Front-End Development. I love developing tools/sites/apps that solve real problems or help make someone's life easier. I like impactful work.</p>
+              <p>I'm a full-stack developer based in Bangladesh. I like creating stuff. Useful and beautiful stuff. Stuff that doesn't just work, but looks and feels good - like a butterfly fluttering on your fingertips.</p>
+
               <h3>Skills</h3>
-              <p>My current skill set includes <strong>JavaScript</strong>, <strong>React</strong>, <strong>Redux</strong>, <strong>Firebase</strong>, <strong>SASS</strong>, <strong>Bootstrap</strong>, <strong>CSS</strong>, <strong>HTML</strong> etc. And that's excluding all the common (yet sometimes complex) build/workflow tools like Webpack, Babel etc.</p>
-              <p>However, I believe in tool agnostic skills. Being in an industry that moves at the speed of time, it sometimes doesn't matter what frameworks/tools I'm familiar with. Because tools/frameworks are so fickle these days that they can become obsolete within months! What matters more is how fast can I learn and make myself comfortable with newer technology. I believe I'm a quick learner. I prioritize concepts over techniques.</p>
+              <p>My current stack consists of <strong>JavaScript</strong>, <strong>TypeScript</strong>, <strong>React</strong>, <strong>Next.js</strong>, <strong>Tailwind CSS</strong>, <strong>Shadcn UI</strong>, <strong>Supabase</strong>, <strong>Firebase</strong>, <strong>SASS</strong>, with foundational things like <strong>CSS</strong>, <strong>HTML</strong> of course.</p>
+
+              <p>However, I believe in tool agnostic, core skills. In an industry that moves faster than Usain Bolt, the choice of frameworks/languages doesn't matter as much. Thanks to all the AI tools, I'm a polyglot now. The ability to learn quickly and be comfortable with newer technology is what matters to me. I'm a quick learner. I prioritize concepts over techniques.</p>
               <p>Simplicity, though overused (and sometimes misused) as a term, is something I really value. I like to build solutions that are simple yet interactive for the users.</p>
               <p>Besides coding, I'm also very passionate about traveling and reading. I feel like the world is a book that was given to me for exploring when I was born, and I don't wanna die before reading all the pages. I plan to become a nomad someday and explore the endless beauty that this planet homes.</p>
             </div>
